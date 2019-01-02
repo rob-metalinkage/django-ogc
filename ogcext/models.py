75ae26e70e54ed6163b03dc002e2c29b60e6a531
Rob Atkinson <rob@metalinkage.com.au>
2019-01-02 12:16:53 +1100

    GML or GMX dictionary options supported

--- ogcext/models.py
+++ ogcext/models.py
@@ -24,7 +24,11 @@ HASTOPCONCEPT_NODE=URIRef('http://www.w3.org/2004/02/skos/core#hasTopConcept')
 MEMBER=URIRef('http://www.w3.org/2004/02/skos/core#member')
 OGC_URN_TYPE = URIRef('http://www.opengis.net/def/metamodel/URN')
 
-
+GMX_MAP = { 'Dictionary': '//gmx:CodeListDictionary' ,
+    'Definition' : '//gmx:CodeDefinition'}
+GML32_MAP= { 'Dictionary': '//gml:Dictionary' ,
+  'Definition' : '//gml:Definition'}
+  
 class GMLDict(ImportedConceptScheme):
 
     def save(self,*args,**kwargs):  
@@ -35,7 +39,12 @@ class GMLDict(ImportedConceptScheme):
             tree = etree.parse(self.file.name)
         elif self.remote :
             tree = etree.parse(self.remote)
-        self.savedgraph = self.extract_gmx_as_skos(tree)
+        if tree.getroot().nsmap.get('gmx') :
+            map=GMX_MAP
+        elif tree.getroot().nsmap.get('gml') == 'http://www.opengis.net/gml/3.2' :
+            map=GML32_MAP
+            
+        self.savedgraph = self.extract_gmx_as_skos(tree, map)    
         scheme_obj = self.importSchemes(self.get_graph(),self.target_scheme, self.force_refresh)
             # update any references to imported schemes
             # print(self.schemes.all())
@@ -45,32 +54,68 @@ class GMLDict(ImportedConceptScheme):
 
     pass
     
-    def extract_gmx_as_skos(self,tree):
+    def extract_gmx_as_skos(self,tree,xpaths):
+        """Extract a GML or GMX dictionary as a SKOS RDf model"""
         nsmap= { 'gml': 'http://www.opengis.net/gml/3.2'  , 'gmx' : 'http://www.isotc211.org/2005/gmx' }
         tgt = self.target_scheme
         if not tgt :
-            tgt = tree.getroot().nsmap[None]
+            # look for Dictionary element and get its identifier
+            try:
+                tgt = self.get_id(None,tree.getroot().xpath(xpaths['Dictionary'],namespaces=nsmap)[0], nsmap)
+            except:
+                try:
+                    # use dfault namespace of XML document if set
+                    tgt = tree.getroot().nsmap[None]
+                except:
+                    raise Exception("Cannot determine target namespace for Dictionary")
+                    
             self.target_scheme = tgt
-        uri = URIRef(tgt)
+        uri = URIRef(tgt)    
         graph = None
-        for codelist in tree.xpath('//gmx:CodeListDictionary',namespaces=nsmap):
+        for codelist in tree.xpath(xpaths['Dictionary'],namespaces=nsmap):
             if not graph :
                 graph = Graph()
                 graph.namespace_manager.bind('skos', Namespace('http://www.w3.org/2004/02/skos/core#'), override=False)
-            id = codelist.xpath('./@gml:id',namespaces=nsmap)[0]
+            id = self.get_id(tgt,codelist,nsmap) 
             if not tgt.endswith(id):
                 uri = URIRef("/".join((tgt,id)) )
             graph.add ( (uri, RDFTYPE_NODE , SCHEME_NODE) )
             graph.add ( (uri, RDFSLABEL_NODE , Literal(id) ) )
-            desc = codelist.xpath('./gml:description',namespaces=nsmap)[0].text
-            graph.add ( (uri, DEFINITION_NODE , Literal(desc) ) )
+            try:
+                desc = codelist.xpath('./gml:description',namespaces=nsmap)[0].text
+                graph.add ( (uri, DEFINITION_NODE , Literal(desc) ) )
+            except:
+                pass
             for code in codelist.xpath('./gml:identifier',namespaces=nsmap):
+                    
                    graph.add ( (uri, NOTATION_NODE , Literal(code.text, datatype=OGC_URN_TYPE ) ) )
-            for concept in codelist.xpath('//gmx:CodeDefinition',namespaces=nsmap):
-                id = concept.xpath('./@gml:id',namespaces=nsmap)[0]
-                duri = URIRef("/".join((uri,id)) )
+            for concept in codelist.xpath(xpaths['Definition'],namespaces=nsmap):
+                id = self.get_id(tgt,concept,nsmap)
+                duri = URIRef(id )
                 graph.add ( (duri, RDFTYPE_NODE  , CONCEPT_NODE ) )
-                desc = concept.xpath('./gml:description',namespaces=nsmap)[0].text
-                graph.add ( (duri, DEFINITION_NODE , Literal(desc) ) )
+                try:
+                    desc = concept.xpath('./gml:description',namespaces=nsmap)[0].text
+                    graph.add ( (duri, DEFINITION_NODE , Literal(desc) ) )
+                except:
+                    pass
+                try:
+                    desc = concept.xpath('./gml:name',namespaces=nsmap)[0].text
+                except:
+                    desc = id
+                graph.add ( (duri, RDFSLABEL_NODE , Literal(desc) ) )
             #print codelist.find('./gml:description',namespaces=nsmap).text
         return graph
+       
+    def get_id(self,uribase,node,nsmap): 
+        gmlid= node.xpath('./gml:identifier',namespaces=nsmap)
+        if gmlid:
+            id = gmlid[0].xpath('@codeSpace')[0]
+            if not id[:-1] in '#/':
+                id = id +'/' 
+            id = id + gmlid[0].text
+        else:
+            id = tgt
+            if not id[:-1] in '#/':
+                id = id +'/' 
+            id = id + node.xpath('./@gml:id',namespaces=nsmap)[0]
+        return id
